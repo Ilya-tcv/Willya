e62f9a3670f2b461744252100474ca0625301eec
William <williamthommeret@yahoo.be>
2020-04-21 16:18:50 +0200

    model

--- app/Projet.php
+++ app/Projet.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Projet extends Model
 {
-    //
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function taches(){
+        return $this->hasMany(Tache::class);
+    }
 }
